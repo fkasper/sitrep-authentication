be6f9d7e8a871469b2759181319939a29d97333e
Florian Kasper <florian@xpandmmi.com>
2016-01-06 09:59:10 +0100

    updated naming

--- scripts/auth-gcloud.sh
+++ scripts/auth-gcloud.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
-docker build -t $EXTERNAL_REGISTRY_ENDPOINT/$PROJECT_NAME:$CIRCLE_SHA1 .
-mkdir -p ~/docker; docker save $EXTERNAL_REGISTRY_ENDPOINT/$PROJECT_NAME:$CIRCLE_SHA1 > ~/docker/image.tar
 
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
-
-
-
-gcloud docker push $EXTERNAL_REGISTRY_ENDPOINT/$PROJECT_NAME > /dev/null
