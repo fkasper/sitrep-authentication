234986a3adf963208db0eeeba33e4363e1a95e2c
Florian Kasper <florian@xpandmmi.com>
2016-01-06 10:12:00 +0100

    updated naming

--- scripts/auth-gcloud.sh
+++ scripts/auth-gcloud.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
+cd $HOME/sitrep-builds/src/github.com/fkasper/sitrep-biometrics
 
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+
+gcloud docker push $EXTERNAL_REGISTRY_ENDPOINT/$PROJECT_NAME > /dev/null
